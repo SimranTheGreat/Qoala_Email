e4742976528a2e1641b0426207515e60abf4ed13
Simran <simrangupta45300@gmail.com>
2026-09-17 15:23:17 +0530

    deployement in github

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/Qoala_Email/",
   plugins: [react(), tailwindcss()],
 });
